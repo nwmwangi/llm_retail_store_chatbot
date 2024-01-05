3c63603626fcac78f0894471293e8633694d15d4
marthawm <mwangimartha808@gmail.com>
2024-01-05 10:26:01 +0300

    create the streamlit user interface for the chatbot

---
+++ main.py
@@ -0,0 +1,13 @@
+# from retail_store_chatbot import get_few_shot_db_chain
+
+import streamlit as st
+
+st.title("Chat with the Retail Store")
+
+question = st.text_input("Question: ")
+if question:
+	pass
+	# chain = get_few_shot_db_chain()
+	# answer = chain.run(question)
+	# st.header("Answer:")
+	# st.write(answer)
